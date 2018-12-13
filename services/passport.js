837ec04c20a5a040f4be154288471dbc95538417
dan00001 <mircea.dan.maduta@gmail.com>
2018-12-13 15:37:13 +0100

    finished auth flow

---
+++ services/passport.js
@@ -0,0 +1,55 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const user = mongoose.model('User');
+
+passport.serializeUser((User, done) => {
+    done(null, User.id);
+});
+
+passport.deserializeUser((id, done) => {
+    user.findById(id)
+        .then(User => {
+            done(null, User);
+        });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        user.findOne({ googleId: profile.id})
+            .then((existingUser) => {
+                if (existingUser) {
+                    done(null, existingUser);
+                } else {
+                    new user({ googleId: profile.id, displayName: profile.displayName})
+                        .save()
+                        .then(newUser => done(null, newUser));
+                }
+            });
+
+        
+    })
+);
+
+passport.use(new FacebookStrategy({
+    clientID: keys.facebookClientID,
+    clientSecret: keys.facebookClientSecret,
+    callbackURL: '/auth/facebook/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    user.findOne({ facebookId: profile.id})
+        .then((existingUser) => {
+            if (existingUser) {
+                done(null, existingUser);
+            } else {
+                new user({ facebookId: profile.id, displayName: profile.displayName})
+                .save()
+                .then(newUser => done(null, newUser));
+            }
+        })
+}))
