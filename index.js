ac0add3fbc232dc018d92896fa266bb2259fa077
dan00001 <mircea.dan.maduta@gmail.com>
2018-12-16 19:27:32 +0100

    change typo

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ require("./routes/authRoutes")(app);
 require("./routes/blogsRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.statis("client/build"));
+  app.use(express.static("client/build"));
   const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
