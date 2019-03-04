3e0d174ce4d93b35c6f2c5abbbe66f44ee9286ae
dan00001 <mircea.dan.maduta@gmail.com>
2019-03-04 11:56:43 +0100

    new update

--- tests/setup.js
+++
@@ -1,9 +0,0 @@
-jest.setTimeout(30000);
-
-require("../models/Users");
-
-const mongoose = require("mongoose");
-const keys = require("../config/keys");
-
-mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI);
